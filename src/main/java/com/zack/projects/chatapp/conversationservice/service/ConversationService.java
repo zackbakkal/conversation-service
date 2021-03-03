63e749021c84783a958b30a85ed73d65d695338f
zackbakkal <zackbakkal@gmail.com>
2021-03-02 22:16:16 -0700

    UI and security added

--- src/main/java/com/zack/projects/chatapp/conversationservice/service/ConversationService.java
+++ src/main/java/com/zack/projects/chatapp/conversationservice/service/ConversationService.java
@@ -35,14 +35,46 @@ public class ConversationService {
             Conversation conversation;
             Timestamp dateStarted = new Timestamp(System.currentTimeMillis());
 
-            log.info(String.format
-                    ("Retrieving conversation between [%s] and [%s]", userA, userB));
-
+            log.info(String.format("Retrieving conversation between [%s] and [%s]", userA, userB));
             conversation = conversationRepository.findConversationByConversationId(userA, userB);
 
             if(conversation != null) {
                 return restTemplate.getForObject(
                         "http://MESSAGE-SERVICE/messages/load/" + userA + "/" + userB, List.class);
+            }
+
+            log.info(String.format
+                    ("Conversation between [%s] and [%s] does not exist yet, " +
+                            "creating one and returning an empty list of messages", userA, userB));
+
+            conversation = new Conversation(userA, userB, dateStarted);
+            conversationRepository.save(conversation);
+            return new ArrayList<>();
+        }
+
+        return null;
+
+    }
+
+    public Collection<Message> getMessagesByNumber(String userA, String userB, int from, int to) {
+
+        boolean userAIsRegistered =
+                restTemplate.getForObject("http://USER-SERVICE/users/registered/" + userA, Boolean.class);
+
+        boolean userBIsRegistered =
+                restTemplate.getForObject("http://USER-SERVICE/users/registered/" + userB, Boolean.class);
+
+        if(userAIsRegistered && userBIsRegistered) {
+            Conversation conversation;
+            Timestamp dateStarted = new Timestamp(System.currentTimeMillis());
+
+            log.info(String.format("Retrieving conversation between [%s] and [%s]", userA, userB));
+            conversation = conversationRepository.findConversationByConversationId(userA, userB);
+
+            if(conversation != null) {
+                return restTemplate.getForObject(
+                        "http://MESSAGE-SERVICE/messages/load/" + userA + "/" + userB + "/" + from + "/" + to,
+                        List.class);
             }
 
             log.info(String.format
